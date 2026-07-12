dec1ca4f60f490e962f2b9bdb452aa368c858bf8
Justin.dev <justinbina515@gmail.com>
2026-07-12 19:31:55 +0000

    Implement responsive GridView in HomeScreen using LayoutBuilder

--- app_recette_certification/lib/screens/home_screen.dart
+++ app_recette_certification/lib/screens/home_screen.dart
@@ -11,92 +11,127 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final recette=ref.watch(recetteProvider);
+    final recette = ref.watch(recetteProvider);
     if (kDebugMode) {
       print(recette);
     }
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Tous les Recettes",style: TextStyle(fontSize: 30,)),
-          actions: [
-            ValueListenableBuilder<ThemeMode>(
-              valueListenable: themeNotifier,
-              builder: (context, mode, child) {
-                return IconButton(
-                  icon: Icon(mode == ThemeMode.light
-                      ? Icons.dark_mode
-                      : Icons.light_mode),
-                  onPressed: () {
-                    themeNotifier.value = mode == ThemeMode.light
-                        ? ThemeMode.dark
-                        : ThemeMode.light;
-                  },
-                  tooltip: 'Toggle Theme',
-                );
-              },
-            ),
-            IconButton(onPressed: () {  }, icon:const Icon(Icons.error_outline_outlined),)
-          ],
-
-        ),
-        body: Container(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: "Rechercher une recette....",
-                        suffixIcon: const Icon(Icons.search),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
+      appBar: AppBar(
+        title: const Text("Tous les Recettes", style: TextStyle(fontSize: 30)),
+        actions: [
+          ValueListenableBuilder<ThemeMode>(
+            valueListenable: themeNotifier,
+            builder: (context, mode, child) {
+              return IconButton(
+                icon: Icon(mode == ThemeMode.light
+                    ? Icons.dark_mode
+                    : Icons.light_mode),
+                onPressed: () {
+                  themeNotifier.value = mode == ThemeMode.light
+                      ? ThemeMode.dark
+                      : ThemeMode.light;
+                },
+                tooltip: 'Toggle Theme',
+              );
+            },
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.error_outline_outlined),
+          )
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Rechercher une recette....",
+                      suffixIcon: const Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
                   ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 4),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Flex(
+                spacing: 5,
+                direction: Axis.horizontal,
+                children: [
+                  FilterButton(
+                    title: 'Tous',
+                    onPressed: () {
+                      ref.read(categorieProvider.notifier).state = "Tous";
+                    },
+                  ),
+                  FilterButton(
+                    title: 'Petit-déjeuner',
+                    onPressed: () {
+                      ref.read(categorieProvider.notifier).state =
+                          "Petit-déjeuner";
+                    },
+                  ),
+                  FilterButton(
+                    title: 'Déjeuner',
+                    onPressed: () {
+                      ref.read(categorieProvider.notifier).state = "Déjeuner";
+                    },
+                  ),
+                  FilterButton(
+                    title: 'Dîner',
+                    onPressed: () {
+                      ref.read(categorieProvider.notifier).state = "Dîner";
+                    },
+                  ),
+                  FilterButton(
+                    title: 'Collation',
+                    onPressed: () {
+                      ref.read(categorieProvider.notifier).state = "Collation";
+                    },
+                  ),
                 ],
               ),
-              const SizedBox(height: 4,),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Flex(
-                    spacing:5,
-                    direction: Axis.horizontal,
-                    children:[
-                      FilterButton(title: 'Tous', onPressed: () {
-                        ref.read(categorieProvider.notifier).state="Tous";
-                      },),
-                      FilterButton(title: 'Petit-déjeuner', onPressed: () {
-                        ref.read(categorieProvider.notifier).state="Petit-déjeuner";
-                      },),
-                      FilterButton(title: 'Déjeuner', onPressed: () {
-                        ref.read(categorieProvider.notifier).state="Déjeuner";
-                      },),
-                      FilterButton(title: 'Dîner', onPressed: () {
-                        ref.read(categorieProvider.notifier).state="Dîner";
-                      },),
-                      FilterButton(title: 'Collation', onPressed: () {
-                        ref.read(categorieProvider.notifier).state="Collation";
-                      },),
+            ),
+            const SizedBox(height: 4),
+            Expanded(
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  int crossAxisCount = 1;
+                  if (constraints.maxWidth > 900) {
+                    crossAxisCount = 3;
+                  } else if (constraints.maxWidth > 600) {
+                    crossAxisCount = 2;
+                  }
 
-                    ]
-                ),
-              ),
-              const SizedBox(height: 4,),
-              Expanded(
-                child: ListView.separated(
+                  return GridView.builder(
                     itemCount: 20,
-                    separatorBuilder: (context, index) => const Divider(),
-                    itemBuilder: (context, index) => ListTile(
-                        onTap: (){
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: crossAxisCount,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      childAspectRatio: crossAxisCount == 1 ? 3 : 1,
+                    ),
+                    itemBuilder: (context, index) => Card(
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: ListTile(
+                        onTap: () {
                           context.push('/detail');
                         },
                         leading: Container(
                           height: 100,
-                          width: 100,
-                          padding: const EdgeInsets.all(10),
+                          width: 60,
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
                             color: Colors.blueGrey,
@@ -104,38 +139,40 @@ class HomeScreen extends ConsumerWidget {
                         ),
                         title: const Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Text("Ayimolou"),
-                            Text("Catégorie"),
+                            Text("Ayimolou", style: TextStyle(fontWeight: FontWeight.bold)),
+                            Text("Catégorie", style: TextStyle(color: Colors.grey, fontSize: 12)),
                             Row(
                               children: [
-                                Icon(Icons.star),
-                                Icon(Icons.star),
-                                Icon(Icons.star),
-                                Icon(Icons.star_half),
-                                Icon(Icons.star_border),
+                                Icon(Icons.star, size: 16, color: Colors.amber),
+                                Icon(Icons.star, size: 16, color: Colors.amber),
+                                Icon(Icons.star, size: 16, color: Colors.amber),
+                                Icon(Icons.star_half, size: 16, color: Colors.amber),
+                                Icon(Icons.star_border, size: 16, color: Colors.amber),
                               ],
                             )
                           ],
                         ),
-                        trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              IconButton(onPressed: (){
-                                context.push('/detail');
-                              }, icon: const Icon(Icons.arrow_forward_ios_rounded)),
-                            ]
-                        )
-                    )
-                ),
-              )
-            ],
-          ),
+                        trailing: IconButton(
+                          onPressed: () {
+                            context.push('/detail');
+                          },
+                          icon: const Icon(Icons.arrow_forward_ios_rounded, size: 16),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
