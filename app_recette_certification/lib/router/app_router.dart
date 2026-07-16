fe3dc47f0d9c0006a55edcfa9b5d1c81322fd7cf
Justin.dev <justinbina515@gmail.com>
2026-07-16 20:04:31 +0000

    refactor: create dedicated app_router.dart and add SettingsScreen route

---
+++ app_recette_certification/lib/router/app_router.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../models/recipe.dart';
+import '../screens/about_screen.dart';
+import '../screens/add_recipe_screen.dart';
+import '../screens/home_screen.dart';
+import '../screens/recipe_detail_screen.dart';
+import '../screens/settings_screen.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: "/detail",
+      builder: (context, state) {
+        final recipe = state.extra as Recipe;
+        return RecipeDetailScreen(recipe: recipe);
+      },
+    ),
+    GoRoute(
+      path: '/add-recipe',
+      builder: (context, state) => const AddRecipeScreen(),
+    ),
+    GoRoute(
+      path: '/about',
+      builder: (context, state) => const AboutScreen(),
+    ),
+    GoRoute(
+      path: '/settings',
+      builder: (context, state) => const SettingsScreen(),
+    ),
+  ],
+);
