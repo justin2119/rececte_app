d599300fa6d4fa946cea546c4634aababf303185
Justin.dev <justinbina515@gmail.com>
2026-07-16 19:43:09 +0000

    refactor: use RecipeRepository in recipe_provider.dart

--- app_recette_certification/lib/providers/recipe_provider.dart
+++ app_recette_certification/lib/providers/recipe_provider.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod/legacy.dart';
 import '../data/data_recipes.dart';
 import '../models/recipe.dart';
+import '../repositories/recipe_repository.dart';
+
+final recipeRepositoryProvider = Provider<RecipeRepository>((ref) {
+  return LocalRecipeRepository(dataRecipes);
+});
 
 final categorieProvider = StateProvider<String?>((ref) => null);
 final searchProvider = StateProvider<String>((ref) => "");
@@ -9,7 +13,7 @@ final searchProvider = StateProvider<String>((ref) => "");
 class RecipeNotifier extends Notifier<List<Recipe>> {
   @override
   List<Recipe> build() {
-    return dataRecipes;
+    return ref.watch(recipeRepositoryProvider).getRecipes();
   }
 
   void addRecipe(Recipe recipe) {
