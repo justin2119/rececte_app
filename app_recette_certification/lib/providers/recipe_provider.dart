9e497f420e790ff0709211294440c7ce89a663ad
Justin.dev <justinbina515@gmail.com>
2026-07-17 07:03:51 +0000

    refactor: remove legacy riverpod import in recipe_provider.dart

--- app_recette_certification/lib/providers/recipe_provider.dart
+++ app_recette_certification/lib/providers/recipe_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod/legacy.dart';
 import '../models/recipe.dart';
 import '../repositories/i_recipe_repository.dart';
 import '../repositories/recipe_repository_impl.dart';
