f9f8aacfd6b80865605f768df30b71e4b0770c56
Justin.dev <justinbina515@gmail.com>
2026-07-17 07:04:21 +0000

    chore: move unit_test to proper location in project structure

---
+++ app_recette_certification/test/unit_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Recipe Unit Tests', () {
+    test('Placeholder unit test for certification', () {
+      expect(1 + 1, 2);
+    });
+  });
+}
